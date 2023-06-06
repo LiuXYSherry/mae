4afd02a174817acb2f5d6eb0f96bbe7c9faa24bb
LiuXYSherry <42861701+LiuXYSherry@users.noreply.github.com>
2023-06-06 16:37:29 -0400

    Update main_pretrain.py

--- main_pretrain.py
+++ main_pretrain.py
@@ -125,6 +125,7 @@ def main(args):
             transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+    # get the batch data with augmentation on the fly
     dataset_train = datasets.ImageFolder(os.path.join(args.data_path, 'train'), transform=transform_train)
     print(dataset_train)
 
